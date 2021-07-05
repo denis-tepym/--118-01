20f26396fcb277e0b5475f5db9704d8c533482b1
denis-tepym <63797243+denis-tepym@users.noreply.github.com>
2021-07-05 13:08:38 +0300

    Add files via upload

---
+++ src/WindowsFormsApplication/WindowsFormsApplication/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApplication
+{
+
+    public partial class Form1 : Form
+    {
+        string zxc;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                webBrowser1.Navigate("about:blank");
+                zxc = openFileDialog1.FileName;
+                webBrowser1.Navigate(zxc);
+                this.Size = new Size(1200, 800);
+                webBrowser1.Visible = true;
+                webBrowser1.Size = new Size(1000, 300);
+                label1.Visible = true;
+                label2.Visible = true;
+                textBox1.Visible = true;
+                textBox2.Visible = true;
+                button1.Visible = true;
+            }
+            else
+            {
+                MessageBox.Show("Ошибка!");
+            }
+
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            О_программе newForm = new О_программе();
+            newForm.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double X = Convert.ToDouble(textBox1.Text);
+            double Y = Convert.ToDouble(textBox2.Text);
+
+            if (((X <=5) && (X >=-2)) && ((Y <= 4) && (Y>=-3)))
+            {
+                MessageBox.Show("Поздравляю, все прошло успешно. Точка лежит в границе!");
+                toolStripStatusLabel1.Text = "Поздравляю, все прошло успешно. Точка лежит в границе!";
+            }
+            else
+            {
+                MessageBox.Show("Извините, заданная вами точка, не лежит в закрашенной области!");
+                toolStripStatusLabel1.Text = "Извините, заданная вами точка, не лежит в закрашенной области!";
+            }
+        }
+
+        private void textBox1_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != ',') && (e.KeyChar != '-'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != ',') && (e.KeyChar != '-'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ src/WindowsFormsApplication/WindowsFormsApplication/О программе.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WindowsFormsApplication
+{
+    partial class О_программе
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(33, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(807, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Работу выполнил: студент группы ПКсп-118, Тимофеев Денис Андреевич, Вариант №13";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(337, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Закрыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // О_программе
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "О_программе";
+            this.Text = "О_программе";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
